fabc360d4350967c93e6eb0ea8e5248ead17047c
Brian Cardarella <bcardarella@gmail.com>
2012-12-02 02:36:04 -0500

    Updated callbacks for new easy_auth

--- lib/easy_auth/oauth2/controllers/sessions.rb
+++ lib/easy_auth/oauth2/controllers/sessions.rb
@@ -2,15 +2,15 @@ module EasyAuth::Oauth2::Controllers::Sessions
 
   private
 
-  def after_successful_sign_in_with_oauth2(identity)
-    send("after_successful_sign_in_with_oauth2_for_#{params[:provider]}", identity)
+  def after_successful_sign_in_with_oauth2
+    send("after_successful_sign_in_with_oauth2_for_#{params[:provider]}")
   end
 
-  def after_successful_sign_in_url_with_oauth2(identity)
-    send("after_successful_sign_in_url_with_oauth2_for_#{params[:provider]}", identity)
+  def after_successful_sign_in_url_with_oauth2
+    send("after_successful_sign_in_url_with_oauth2_for_#{params[:provider]}")
   end
 
-  def after_failed_sign_in_with_oauth2(identity)
-    send("after_failed_sign_in_with_oauth2_for_#{params[:provider]}", identity)
+  def after_failed_sign_in_with_oauth2
+    send("after_failed_sign_in_with_oauth2_for_#{params[:provider]}")
   end
 end
