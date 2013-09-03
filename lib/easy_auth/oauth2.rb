4d215cc8e9deb1ca1353992be43eed7f21156207
Nicolás Hock Isaza <nhocki@gmail.com>
2013-09-02 21:38:10 -0500

    Use const_get instead of eval

--- lib/easy_auth/oauth2.rb
+++ lib/easy_auth/oauth2.rb
@@ -30,8 +30,8 @@ module EasyAuth
     camelcased_provider_name = params[:provider].to_s.camelcase
     if respond_to?(method_name)
       send(method_name, params)
-    elsif eval("::Identities::Oauth2::#{camelcased_provider_name} rescue nil")
-      eval("::Identities::Oauth2::#{camelcased_provider_name}")
+    elsif const_defined? "::Identities::Oauth2::#{camelcased_provider_name}".to_sym
+      const_get("::Identities::Oauth2::#{camelcased_provider_name}")
     else
       camelcased_provider_name.constantize
     end
