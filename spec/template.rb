5ef6c4b3801949e910c3a489d876e9ad3cd9a516
Stas SUȘCOV <stas@net.utcluj.ro>
2013-10-23 22:17:35 +0300

    Drop database before any db changes are done.

--- spec/template.rb
+++ spec/template.rb
@@ -1,5 +1,3 @@
-generate('easy_auth:setup')
-
 file 'config/database.yml', :force => true do
 <<-CONFIG
 test:
@@ -21,6 +19,11 @@ development:
 CONFIG
 end
 
+inside (ENV['DUMMY_APP_PATH'] || 'spec/dummy') do
+  rake('db:drop')
+end
+
+generate('easy_auth:setup')
 generate(:model, 'user email:string session_token:string -t false')
 
 insert_into_file 'app/models/user.rb', :after => "class User < ActiveRecord::Base\n" do
